8687a70b140f412e0103da7019a1cf072a2c7a3b
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-04-06 17:19:04 -0600

    users starter route

---
+++ backend/routes/users.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const { getUsers } = require('../db/user-queries');
+
+// use bcrypt to hash passwords (we might have to add the other bcrypt for nolan)
+// const bcrypt = require('bcrypt');
+
+router.get('/', (req, res) => {
+	getUsers().then((usersData) => {
+		res.json(usersData.rows);
+	});
+});
+
+module.exports = router;
