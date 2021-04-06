7c461bd7c94577f4cabe94f9e5aeaea732d75fbb
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-04-06 17:19:44 -0600

    initial users db table

---
+++ backend/db/seeds/01_users_seed.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (first_name, last_name, email, password) VALUES
+('Hosam', 'Dahrooge', 'hosam@test.com', 'password'),
+('Nolan', 'Eckert', 'nolan@test.com', 'password'),
+('Kyle', 'Lemmon', 'kyle@test.com', 'password');
