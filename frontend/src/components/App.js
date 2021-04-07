ed7650ad09c980ead1db56ab83eaae062fddd13c
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-04-06 19:43:02 -0600

    front end setup

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,9 +1,10 @@
-function App() {
+const App = () => {
 	return (
 		<div className="App">
 			<h1>Hello World! The react app is up!</h1>
+			<button>HI</button>
 		</div>
 	);
-}
+};
 
 export default App;
