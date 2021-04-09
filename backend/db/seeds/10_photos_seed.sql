fe7898ac1b294679dfadc466b4fe6c099175b930
Nolan Eckert <nolan.eckert@gmail.com>
2021-04-08 22:39:27 -0600

    Add seeds for photos table.

---
+++ backend/db/seeds/10_photos_seed.sql
@@ -0,0 +1,10 @@
+INSERT INTO dashboards (dashboard_id, img_url, text) VALUES
+(1, 'https://unsplash.com/photos/fdlZBWIP0aM', 'text1.1'),
+(1, 'https://unsplash.com/photos/RN6ts8IZ4_0', 'text1.2'),
+(1, 'https://unsplash.com/photos/Pf6e3o0GL4M', 'text1.3'),
+(2, 'https://unsplash.com/photos/zcUgjyqEwe8', 'text2.1'),
+(2, 'https://unsplash.com/photos/TWoL-QCZubY', 'text2.2'),
+(2, 'https://unsplash.com/photos/iu1b3S-ZV2Q', 'text2.3'),
+(3, 'https://unsplash.com/photos/pLKgCsBOiw4', 'text3.1');
+(3, 'https://unsplash.com/photos/HN9X7gNreqA', 'text3.2');
+(3, 'https://unsplash.com/photos/h4i9G-de7Po', 'text3.3');
