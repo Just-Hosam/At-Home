5e1a6f406d66156784498d9f8b1453e082c16d19
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-04-08 19:57:49 -0600

    query for users seed

--- backend/db/seeds/01_users_seed.sql
+++ backend/db/seeds/01_users_seed.sql
@@ -1,4 +1,7 @@
 INSERT INTO users (first_name, last_name, email, password) VALUES
-('Hosam', 'Dahrooge', 'hosam@test.com', 'password'),
-('Nolan', 'Eckert', 'nolan@test.com', 'password'),
-('Kyle', 'Lemmon', 'kyle@test.com', 'password');
+('Hosam', 'Dahrooge', 'hosam@test.com', 'test'),
+('Nolan', 'Eckert', 'nolan@test.com', 'test'),
+('Kyle', 'Lemmon', 'kyle@test.com', 'test'),
+('Random1', 'Child', 'child1@test.com', 'test'),
+('Random2', 'Child', 'child2@test.com', 'test'),
+('Random3', 'Child', 'child3@test.com', 'test');
