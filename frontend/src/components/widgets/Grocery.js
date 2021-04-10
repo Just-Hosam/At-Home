0fd5d3ce39b3c1236c3bd548fac1fe6bd549efee
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-04-10 09:38:35 -0600

    prettier reactivation

--- frontend/src/components/widgets/Grocery.js
+++ frontend/src/components/widgets/Grocery.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from 'react';
 
 export default function Grocery(props) {
-  return (
-    <div id='grocery-item'>
-      <p>{props.item}</p>
-      <button onClick={() => props.onClick(props.dashboardId, props.itemId)}>X</button>
-    </div>
-  )
+	return (
+		<div id="grocery-item">
+			<p>{props.item}</p>
+			<button onClick={() => props.onClick(props.dashboardId, props.itemId)}>
+				X
+			</button>
+		</div>
+	);
 }
