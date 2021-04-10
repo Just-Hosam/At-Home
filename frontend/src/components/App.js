ae1f82194f5d9ebbee496b31ba90ad031c7a7714
Nolan Eckert <nolan.eckert@gmail.com>
2021-04-10 10:25:54 -0600

    Initial material-ui photo gallery import.

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,18 +1,23 @@
-import Groceries from './widgets/Groceries';
-import usePolling from "../hooks/usePolling";
-import parsePollingData from "./Polling/parsePollingData";
+// import Groceries from './widgets/Groceries';
+// import usePolling from "../hooks/usePolling";
+// import parsePollingData from "./Polling/parsePollingData";
+import Photos from './widgets/Photos';
+import Button from '@material-ui/core/Button';
 
 const App = () => {
 
 	// fetches current polling state from custom hook
-  const {state} = usePolling();
-	const pollingData =  parsePollingData(state.polls);
-	const pollingWidget = pollingData ? pollingData : null;
+  // const {state} = usePolling();
+	// const pollingData =  parsePollingData(state.polls);
+	// const pollingWidget = pollingData ? pollingData : null;
 
 	
 	return (
 		<div className="App">
-			{pollingWidget}
+			{/* {pollingWidget} */}
+			<Button variant="contained">Default</Button>
+			<h1>hi</h1>
+			<Photos />
 		</div>
 	);
 };
