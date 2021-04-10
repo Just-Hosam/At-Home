1b2ab1568bbc5b9937622dbe8fd2a0cd8be62874
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-04-10 10:24:06 -0600

    groceries component testing

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,18 +1,16 @@
-// import Groceries from './widgets/Groceries';
-import usePolling from "../hooks/usePolling";
-import parsePollingData from "./Polling/parsePollingData";
+import Groceries from './widgets/Groceries';
+// import usePolling from "../hooks/usePolling";
+// import parsePollingData from "./Polling/parsePollingData";
 
 const App = () => {
-
 	//fetches current polling state from custom hook
-  const {state} = usePolling();
-	const pollingData =  parsePollingData(state.polls);
-	const pollingWidget = pollingData ? pollingData : null;
+	// const {state} = usePolling();
+	// const pollingData =  parsePollingData(state.polls);
+	// const pollingWidget = pollingData ? pollingData : null;
 
-	
 	return (
 		<div className="App">
-		{pollingWidget}
+			<Groceries />
 		</div>
 	);
 };
