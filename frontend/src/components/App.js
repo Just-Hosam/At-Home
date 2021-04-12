1a0a4d481e6d9b5101d1eb51c2ee4b0826d2cc37
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-04-12 17:36:32 -0600

    import RecipeMin and RecipeMax

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -3,6 +3,8 @@
 
 // import Photos from './widgets/Photos';
 // import Gallery from './widgets/Gallery';
+// import RecipesMin from './widgets/Recipes/RecipesMin';
+// import RecipeMax from './widgets/Recipes/RecipeMax';
 
 const App = () => {
 	return <div className="App"></div>;
