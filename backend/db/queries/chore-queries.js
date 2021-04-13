74b68f74f4bc86b535d8dbf5c30e1d0b8d34cb63
Nolan Eckert <nolan.eckert@gmail.com>
2021-04-13 14:21:37 -0600

    Add basic chore queries.

---
+++ backend/db/queries/chore-queries.js
@@ -0,0 +1,77 @@
+const db = require("../../lib/db.js");
+
+const getChores = (dashboardId) => {
+  const text = `
+  SELECT * 
+  FROM chores
+  WHERE dashboard_id = $1;`;
+  const values = [dashboardId];
+
+  return db
+    .query(text, values)
+    .then((res) => res.rows)
+    .catch((err) => console.log('Error at chores queries "getChores"', err));
+};
+
+const getChore = (dashboardId, choreId) => {
+  const text = `
+	SELECT *
+	FROM chores
+	WHERE dashboard_id = $1
+	AND id = $2;`;
+  const values = [dashboardId, choreId];
+
+  return db
+    .query(text, values)
+    .then((res) => res.rows[0])
+    .catch((err) => console.log('Error at chores queries "getChore"', err));
+};
+
+const addChore = (dashboardId, choreText, assignedName) => {
+  const text = `
+  INSERT INTO chores (dashboard_id, text, name)
+	VALUES ($1, $2, $3)
+	RETURNING *;`;
+  const values = [dashboardId, choreText, assignedName];
+
+  return db
+    .query(text, values)
+    .then((res) => res.rows[0])
+    .catch((err) => console.log('Error at chores queries "addChore"', err));
+};
+
+const editChore = (dashboardId, choreId, choreText, assignedName, choreDone) => {
+  const text = `
+	UPDATE chores
+	SET text = $1, name = $2, done = $3
+	WHERE dashboard_id = $4
+	AND id = $5;`;
+  const values = [choreText, assignedName, choreDone, dashboardId, choreId];
+
+  return db
+    .query(text, values)
+    .then((res) => res)
+    .catch((err) => console.log('Error at chores queries "editChore"', err));
+};
+
+const deleteChore = (dashboardId, choreId) => {
+  const text = `
+	DELETE FROM chores
+	WHERE dashboard_id = $1
+	AND id = $2
+	RETURNING *;`;
+  const values = [dashboardId, choreId];
+
+  return db
+    .query(text, values)
+    .then((res) => res)
+    .catch((err) => console.log('Error at chores queries "deleteChore"', err));
+};
+
+module.exports = {
+  getChores,
+  getChore,
+  addChore,
+  editChore,
+  deleteChore,
+};
