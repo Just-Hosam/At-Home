0ffc184cce97f3d47ecf808cb334e6df7d3ff3b2
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-04-13 20:20:16 -0600

    integrate RecipeSend to max component

--- frontend/src/components/widgets/Recipes/RecipeMax.js
+++ frontend/src/components/widgets/Recipes/RecipeMax.js
@@ -3,6 +3,7 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 
 import RecipeShow from './RecipeShow';
 import RecipeEdit from './RecipeEdit';
+import RecipeSend from './RecipeSend';
 
 export default function RecipeMax(props) {
 	const dashboardId = 1;
@@ -25,6 +26,14 @@ export default function RecipeMax(props) {
 			)}
 			{view === 'EDIT' && (
 				<RecipeEdit
+					dashboardId={dashboardId}
+					handleEdit={handleEdit}
+					recipeId={props.recipeId}
+					handleView={props.handleView}
+				/>
+			)}
+			{view === 'SEND' && (
+				<RecipeSend
 					dashboardId={dashboardId}
 					handleEdit={handleEdit}
 					recipeId={props.recipeId}
