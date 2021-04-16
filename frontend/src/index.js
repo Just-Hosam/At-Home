d60faa237f51e95b881ddcf120556e5c1f25a9ec
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-04-16 09:18:56 -0600

    wrap application with cookies provider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { CookiesProvider } from 'react-cookie';
+
 import App from './components/App';
 
 ReactDOM.render(
-	<App />,
+	<CookiesProvider>
+		<App />
+	</CookiesProvider>,
 	document.getElementById('root')
 );
