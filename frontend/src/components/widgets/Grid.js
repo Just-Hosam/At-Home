5f9909bf2a62268bec3e708c02c991d6ee233b62
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-04-17 16:15:50 -0600

    grid adjustments

--- frontend/src/components/widgets/Grid.js
+++ frontend/src/components/widgets/Grid.js
@@ -1,6 +1,6 @@
 import Groceries from './Groceries/Groceries';
 import PollWidget from './Polling/PollsWidget';
-import RecipesMin from './Recipes/RecipesMin';
+import RecipeFinal from './Recipes/RecipeFinal';
 // import RecipeMax from './widgets/Recipes/RecipeMax';
 import GalleryModal from './Photos/GalleryModal';
 // import Photos from './Photos';
@@ -10,22 +10,11 @@ import CalendarWidget from './Calendar/CalendarWidget';
 const Grid = () => {
 	return (
 		<div id="grid-wrapper">
-			<div id="calendar-grid-widget">
-				<CalendarWidget />
-			</div>
-
-			<div id="groceries-grid-widget">
-				<Groceries />
-			</div>
-			<div id="gallery-grid-widget">
-				<GalleryModal />
-			</div>
-			<div id="recipes-grid-widget">
-				<RecipesMin />
-			</div>
-			<div id="polls-grid-widget">
-				<PollWidget />
-			</div>
+			<CalendarWidget />
+			<Groceries />
+			<RecipeFinal />
+			<GalleryModal />
+			<PollWidget />
 			<div id="chores-grid-widget">Chores</div>
 		</div>
 	);
