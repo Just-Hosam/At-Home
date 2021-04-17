6e734df427dbfc00ea9d3915d7535bdd6c6b2a5b
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-04-16 20:37:08 -0600

    temp commit

--- frontend/src/components/widgets/Grid.js
+++ frontend/src/components/widgets/Grid.js
@@ -1,5 +1,4 @@
 import Groceries from './Groceries/Groceries';
-import Dropdown from './Dropdown/Dropdown';
 import PollWidget from './Polling/PollsWidget';
 import RecipesMin from './Recipes/RecipesMin';
 // import RecipeMax from './widgets/Recipes/RecipeMax';
@@ -9,41 +8,27 @@ import GalleryModal from './Photos/GalleryModal';
 import CalendarWidget from './Calendar/CalendarWidget';
 
 const Grid = () => {
-return <div className="grid-wrapper">
+	return (
+		<div id="grid-wrapper">
+			<div id="calendar-grid-widget">
+				<CalendarWidget />
+			</div>
 
-<Dropdown/>
-	
-<div className='grid-stage'>
-
-<div className="grid-container">
-	
-	<div id='calendar-grid-widget' className="widget1"><CalendarWidget/></div>
-	
-	<div id='groceries-grid-widget' className="widget2"><Groceries/></div> 
-
-	<div id='recipes-grid-widget' className="widget3"><RecipesMin/></div>
-
-	<div id='gallery-grid-widget' className="widget4"><GalleryModal/></div>
-
-	<div id='chores-grid-widget' className="widget5">Chores</div>
-	
-	<div id='polls-grid-widget' className="widget6"><PollWidget/></div>
-
-</div>
-
-</div>
-
-</div>;
-
-
-}
-
-
-
-
-
-
-	
-
+			<div id="groceries-grid-widget">
+				<Groceries />
+			</div>
+			<div id="gallery-grid-widget">
+				<GalleryModal />
+			</div>
+			<div id="recipes-grid-widget">
+				<RecipesMin />
+			</div>
+			<div id="polls-grid-widget">
+				<PollWidget />
+			</div>
+			<div id="chores-grid-widget">Chores</div>
+		</div>
+	);
+};
 
 export default Grid;
