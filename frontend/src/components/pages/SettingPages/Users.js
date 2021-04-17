053f079287084b50af4926467df5ed4909b08c90
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-04-17 15:47:04 -0600

    add user to dashboard form

--- frontend/src/components/pages/SettingPages/Users.js
+++ frontend/src/components/pages/SettingPages/Users.js
@@ -5,29 +5,46 @@ import { useCookies } from 'react-cookie';
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
 import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
 
 import axios from 'axios';
 
 export default function Dashboards() {
 	const [cookies] = useCookies(['userID']);
 	const [users, setUsers] = useState([]);
+	const [allUsers, setAllUsers] = useState([]);
 
 	useEffect(() => {
 		axios
 			.get(`/dashboards/${cookies.dashboardId}/users`)
 			.then((res) => {
 				setUsers(res.data);
+				axios
+					.get(`/users`)
+					.then((allUsersRes) => {
+						const filteredUsers = allUsersRes.data.filter((elem) => {
+							let finalAnswer = true;
+							for (const innerElem of res.data) {
+								if (innerElem.user_id === elem.id) finalAnswer = false;
+							}
+							return finalAnswer;
+						});
+						setAllUsers(filteredUsers);
+					})
+					.catch((err) => console.log(err));
 			})
 			.catch((err) => console.log(err));
 	}, []);
 
-	const removeUser = (userId) => {
+	const removeUser = (userObj) => {
 		axios
-			.delete(`/dashboards/${cookies.dashboardId}/users/${userId}`)
+			.delete(`/dashboards/${cookies.dashboardId}/users/${userObj.user_id}`)
 			.then(() => {
 				setUsers((prev) => {
-					return prev.filter((elem) => elem.user_id !== userId);
+					return prev.filter((elem) => elem.user_id !== userObj.user_id);
 				});
+				setAllUsers((prev) => [userObj, ...prev]);
 			})
 			.catch((err) => console.log(err));
 	};
@@ -48,8 +65,6 @@ export default function Dashboards() {
 			.catch((err) => console.log(err));
 	};
 
-	console.log(users);
-
 	const usersList = users.map((elem) => {
 		return (
 			<li key={elem.user_id}>
@@ -61,10 +76,7 @@ export default function Dashboards() {
 					<span>{elem.email}</span>
 				</div>
 				<div>
-					<IconButton
-						className="deletebtn"
-						onClick={() => removeUser(elem.user_id)}
-					>
+					<IconButton className="deletebtn" onClick={() => removeUser(elem)}>
 						<DeleteIcon />
 					</IconButton>
 
@@ -91,9 +103,44 @@ export default function Dashboards() {
 		);
 	});
 
+	const submission = (event) => {
+		event.preventDefault();
+		axios
+			.post(`/dashboards/${cookies.dashboardId}/users`, {
+				userEmail: event.target[0].value,
+			})
+			.then((res) => {
+				setUsers((prev) => {
+					return [res.data, ...prev];
+				});
+				setAllUsers((prev) => {
+					return prev.filter((elem) => elem.id !== res.data.id);
+				});
+			});
+	};
+
 	return (
 		<div id="settings-users">
 			<h3>Users</h3>
+			<form id="add-user-form" onSubmit={(e) => submission(e)}>
+				<Autocomplete
+					id="add-user-search"
+					freeSolo
+					options={allUsers.map((option) => `${option.email}`)}
+					renderInput={(params) => (
+						<TextField
+							{...params}
+							label="Add User"
+							margin="normal"
+							variant="outlined"
+							size="small"
+						/>
+					)}
+				/>
+				<Button type="submit" className="add-btn" variant="contained" fullWidth>
+					Add
+				</Button>
+			</form>
 			<ul>{usersList}</ul>
 		</div>
 	);
