926e231f794a9905127bb810fcd5346adf3765f3
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-04-17 15:58:25 -0600

    Recipes widget modal

---
+++ frontend/src/components/widgets/Recipes/RecipeFinal.js
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+
+import RecipeMin from './RecipesMin';
+import RecipeMax from './RecipeMax';
+import RecipeAdd from './RecipeAdd';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+
+const useStyles = makeStyles((theme) => ({
+	modal: {
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	paper: {
+		backgroundColor: theme.palette.background.paper,
+		borderRadius: 10,
+		boxShadow: theme.shadows[5],
+		outline: 'none',
+	},
+}));
+
+export default function RecipeFinal() {
+	const classes = useStyles();
+	const [open, setOpen] = useState(false);
+
+	const handleOpen = () => {
+		setOpen(true);
+	};
+
+	const handleClose = () => {
+		setOpen(false);
+	};
+
+	const [modalView, setModalView] = useState('');
+	const [itemId, setItemId] = useState(0);
+
+	const handleView = (newView, id) => {
+		setModalView(newView);
+		setItemId(id);
+	};
+
+	return (
+		<div>
+			<RecipeMin handleView={handleView} handleOpen={handleOpen} />
+			{/* <button onClick={handleOpen}>hi</button> */}
+			<Modal
+				className={classes.modal}
+				open={open}
+				onClose={handleClose}
+				closeAfterTransition
+				BackdropComponent={Backdrop}
+				BackdropProps={{
+					timeout: 500,
+				}}
+			>
+				<Fade in={open}>
+					<div className={classes.paper}>
+						{modalView === 'RECIPE_SHOW' && (
+							<RecipeMax
+								recipeId={itemId}
+								handleClose={handleClose}
+								handleView={handleView}
+							/>
+						)}
+						{modalView === 'RECIPE_ADD' && (
+							<RecipeAdd handleClose={handleClose} handleView={handleView} />
+						)}
+					</div>
+				</Fade>
+			</Modal>
+		</div>
+	);
+}
