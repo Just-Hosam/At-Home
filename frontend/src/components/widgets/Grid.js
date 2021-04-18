84194c25fd6de941da95557d40766282fb20e2f2
Nolan Eckert <nolan.eckert@gmail.com>
2021-04-18 17:50:55 -0600

    package-lock and grid conflicts resolved.

--- frontend/src/components/widgets/Grid.js
+++ frontend/src/components/widgets/Grid.js
@@ -3,42 +3,13 @@ import PollWidget from './Polling/PollsWidget';
 import RecipeFinal from './Recipes/RecipeFinal';
 // import RecipeMax from './widgets/Recipes/RecipeMax';
 import GalleryModal from './Photos/GalleryModal';
-<<<<<<< HEAD
-import Chores from './Chores/Chores';
-// import Gallery from './Gallery';
-=======
->>>>>>> main
 import CalendarWidget from './Calendar/CalendarWidget';
+import Chore from './Chores/Chore';
 
 const Grid = () => {
 
 
 
-<<<<<<< HEAD
-	<div id='recipes-grid-widget' className="widget3"><RecipesMin/></div>
-
-	<div id='gallery-grid-widget' className="widget4"><GalleryModal/></div>
-
-	<div id='chores-grid-widget' className="widget5"><Chores/></div>
-	
-	<div id='polls-grid-widget' className="widget6"><PollWidget/></div>
-
-</div>
-
-</div>
-
-</div>;
-
-
-}
-
-
-
-
-
-
-	
-=======
 	return (
 		<div id="grid-wrapper">
 			<CalendarWidget />
@@ -46,11 +17,10 @@ const Grid = () => {
 			<RecipeFinal />
 			<GalleryModal />
 			<PollWidget />
-			<div id="chores-grid-widget">Chores</div>
+			<Chore />
 		</div>
 	);
 };
->>>>>>> main
 
 
 export default Grid;
