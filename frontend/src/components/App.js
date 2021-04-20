8bf55db8d83abba12537566f0035c6dd88aff066
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-04-19 18:35:38 -0600

    themes setup

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,44 +2,51 @@ import React, { useState, useEffect } from 'react';
 import { useCookies } from 'react-cookie';
 import urlParams from '../helpers/urlParams';
 
-
+// Components Import
 import Register from './pages/Register';
 import Login from './pages/Login';
 import Settings from './pages/Settings';
 import Navbar from './widgets/Navbar/Navbar';
 import Grid from './widgets/Grid';
-import axios from 'axios';
-import useSocket from "../hooks/useSocket";
+import useSocket from '../hooks/useSocket';
+
+// Themes Require
+const { lightPurpleTheme } = require('./pages/SettingPages/Themes/lightPurple');
+const { lightBlueTheme } = require('./pages/SettingPages/Themes/lightBlue');
+const { darkYellowTheme } = require('./pages/SettingPages/Themes/darkYellow');
+const { darkPinkTheme } = require('./pages/SettingPages/Themes/darkPink');
 
 const App = () => {
+	//websocket connection
+	const { sendSocketMessage } = useSocket();
+	const [cookies, setCookie] = useCookies(null);
+	const initialPage = cookies.userData ? 'GRID' : 'LOGIN';
+	const [page, setPage] = useState(initialPage);
+	const handlePage = (page) => setPage(page);
 
-  //websocket connection
-	const {sendSocketMessage} = useSocket();
-	const [cookies, setCookie, removeCookie] = useCookies(null);
-  const initialPage = cookies.userData ? 'GRID' : 'LOGIN';
-  const [page, setPage] = useState(initialPage);
-  const handlePage = (page) => setPage(page);
+	if (cookies.theme === 'LIGHT_PURPLE') lightPurpleTheme();
+	if (cookies.theme === 'LIGHT_BLUE') lightBlueTheme();
+	if (cookies.theme === 'DARK_YELLOW') darkYellowTheme();
+	if (cookies.theme === 'DARK_PINK') darkPinkTheme();
 
-  
-	useEffect(() => {      
+	useEffect(() => {
+		const invite = urlParams();
 
-	  const invite = urlParams(); 
-	
-    if(invite){
-      setCookie('dashboardId', invite.id, { path: '/' });
-      sendSocketMessage(invite.email); // <-- call websocket update 
-	  }
-  }, []); 
+		if (invite) {
+			setCookie('dashboardId', invite.id, { path: '/' });
+			sendSocketMessage(invite.email); // <-- call websocket update
+		}
+	}, []);
 
-  return (
-    <div className="App">
-      <Navbar handlePage={handlePage} />
-      {page === 'GRID' && <Grid />}
-      {page === 'SETTINGS' && <Settings handlePage={handlePage} />}
-      {page === 'LOGIN' && <Login handlePage={handlePage} />}
-      {page === 'REGISTER' && <Register handlePage={handlePage} />}
-    </div>
-  );
+	return (
+		<div className="App">
+			<Navbar handlePage={handlePage} />
+			{page === 'GRID' && <Grid />}
+			{page === 'SETTINGS' && <Settings handlePage={handlePage} />}
+			{page === 'LOGIN' && <Login handlePage={handlePage} />}
+			{page === 'REGISTER' && <Register handlePage={handlePage} />}
+		</div>
+	);
 };
 
 export default App;
