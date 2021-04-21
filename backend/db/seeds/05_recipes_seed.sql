17457dfb91dd7afd9cf666e52de94f12a4aefe96
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-04-21 12:31:30 -0600

    recipes seeds

--- backend/db/seeds/05_recipes_seed.sql
+++ backend/db/seeds/05_recipes_seed.sql
@@ -1,11 +1,8 @@
-INSERT INTO recipes (dashboard_id, title, directions, time) VALUES
-(1, 'Nolans Tzatziki', 'Do not burn yourself', '20 mins'),
-(1, 'Fish and Vegetables', 'Do not burn yourself', '30 mins'),
-(1, 'Frozen Pizza', 'Do not burn yourself', '15 mins'),
-(1, 'Nolans Tzatziki', 'Do not burn yourself', '20 mins'),
-(1, 'Fish and Vegetables', 'Do not burn yourself', '30 mins'),
-(1, 'Frozen Pizza', 'Do not burn yourself', '15 mins'),(1, 'Nolans Tzatziki', 'Do not burn yourself', '20 mins'),
-(1, 'Fish and Vegetables', 'Do not burn yourself', '30 mins'),
-(1, 'Frozen Pizza', 'Do not burn yourself', '15 mins'),(1, 'Nolans Tzatziki', 'Do not burn yourself', '20 mins'),
-(1, 'Fish and Vegetables', 'Do not burn yourself', '30 mins'),
-(1, 'Frozen Pizza', 'Do not burn yourself', '15 mins');
+INSERT INTO recipes (dashboard_id, title, directions, time, img_url) VALUES
+(1, 'Nolans Tzatziki', 'Do not burn yourself', '20 mins', 'https://media.fromthegrapevine.com/assets/images/2015/7/tzatziki-sauce-with-pita.jpg.839x0_q71_crop-scale.jpg'),
+(1, 'Bucatini AllAmatriciana', 'Do not burn yourself', '40 mins', 'https://images.pexels.com/photos/6287534/pexels-photo-6287534.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'),
+(1, 'Thai Chicken with Egg', 'Do not burn yourself', '15 mins', 'https://images.pexels.com/photos/3727196/pexels-photo-3727196.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'),
+(1, 'Mushroom Spinach Omelet', 'Do not burn yourself', '20 mins', 'https://images.pexels.com/photos/5639255/pexels-photo-5639255.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'),
+(1, 'Tomato Basil Salmon', 'Do not burn yourself', '30 mins', 'https://images.pexels.com/photos/842142/pexels-photo-842142.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'),
+(1, 'Breakfast Porridge', 'Do not burn yourself', '30 mins', 'https://images.pexels.com/photos/4725743/pexels-photo-4725743.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'),
+(1, 'Italian Lasagna', 'Bring an Italian, let them make the Lasagna. Enjoy!', '3 hours', 'https://images.pexels.com/photos/5949889/pexels-photo-5949889.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
--- backend/db/seeds/06_ingredients_seed.sql
+++ backend/db/seeds/06_ingredients_seed.sql
@@ -1,10 +1,8 @@
 INSERT INTO ingredients (recipe_id, item, measurement) VALUES
-(1, 'Greek Yogurt', '2 cups'),
-(1, 'Grated Cucumber', '1 1/2 cups'),
-(1, 'Lemon Juice', '1 tablespoon'),
-(1, 'Sea Salt', '1/2 teaspoon'),
-(2, 'Fish', '1/2 Kg'),
-(2, 'Sweet Peppers', '1 1/2 PCs'),
-(2, 'Soy Sauce', '2 tablespoons'),
-(3, 'Frozen Pizza', '1 PC'),
-(3, 'Oven', '1 PC');
+(7, 'Lean Ground Beed', '3/4 pound'),
+(7, 'Minced Onions', '1/2 cup'),
+(7, 'Garlic', '2 cloves'),
+(7, 'Tomato Paste', '2 cans'),
+(7, 'Italian Seasoning', '1 teaspoon'),
+(7, 'Lasagna Noodles', '12 pcs'),
+(7, 'Cheese', '3/4 pound');
