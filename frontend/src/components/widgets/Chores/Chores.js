7e44fe7dd087dc334ad37979467c0bbf08b29ed7
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-04-21 12:40:58 -0600

    progress bar correction

--- frontend/src/components/widgets/Chores/Chores.js
+++ frontend/src/components/widgets/Chores/Chores.js
@@ -16,179 +16,191 @@ import MenuItem from '@material-ui/core/MenuItem';
 import Select from '@material-ui/core/Select';
 
 export default function Chores(props) {
-  const [cookies] = useCookies(['userID']);
-  const { broadcast } = useSocket();
-  const dashboardId = cookies.dashboardId;
-  const parentUsers = [...props.choreState.dashboardUsers];
-  const setParentUsers = props.choreState.setDashboardUsers;
-  const parentChores = [...props.choreState.choresList];
-  const setParentChores = props.choreState.setChoresList;
-  const parentDone = [...props.choreState.doneList];
-  const setParentDone = props.choreState.setDoneList;
-  const parentProgress = { ...props.choreState.progress };
-  const setParentProgress = props.choreState.setProgress;
+	const [cookies] = useCookies(['userID']);
+	const { broadcast } = useSocket();
+	const dashboardId = cookies.dashboardId;
 
-  useEffect(() => {
-    axios
-      .get(`/dashboards/${dashboardId}/chores/`)
-      .then((res) => {
-        setParentChores(res.data);
-        setParentProgress({ ...parentProgress, total: res.data.length });
-        setParentDone(res.data.filter((elem) => elem.done));
-        axios.get(`/dashboards/${dashboardId}/users`).then((res) => {
-          setParentUsers(res.data);
-        });
-      })
-      .catch((err) => console.log('CHORES COMPONENT ERROR', err));
-  }, [broadcast.chores]);
+	const parentUsers = [...props.choreState.dashboardUsers];
+	const setParentUsers = props.choreState.setDashboardUsers;
 
-  const handleToggle = (value) => () => {
-    value.done = !value.done;
-    axios
-      .patch(`/dashboards/${dashboardId}/chores/${value.id}`, value)
-      .then((res) => {
-        setParentChores((prev) => {
-          return prev.map((elem) => {
-            if (elem.id === value.id) return res.data;
-            return elem;
-          });
-        });
-      })
-      .then((res) => {
-        setParentDone(parentChores.filter((elem) => elem.done));
-      })
-      .then(() => {
-        setParentProgress({ ...parentProgress, current: parentDone.length });
-      })
-      .catch((err) => console.log('handleToggle ERROR', err));
-  };
+	const parentChores = [...props.choreState.choresList];
+	const setParentChores = props.choreState.setChoresList;
 
-  const updateChore = (dashboardId, chore) => {
-    axios
-      .patch(`/dashboards/${dashboardId}/chores/${chore.id}`, chore)
-      .catch((err) => console.log('ERROR UPDATING CHORE', err));
-  };
+	const parentDone = [...props.choreState.doneList];
+	const setParentDone = props.choreState.setDoneList;
 
-  const clearChip = (chip) => {
-    setParentChores((prev) => {
-      return prev.map((elem) => {
-        if (elem.id === chip.id) {
-          return { ...elem, name: 'none' };
-        }
-        return elem;
-      });
-    });
-  };
+	const parentProgress = { ...props.choreState.progress };
+	const setParentProgress = props.choreState.setProgress;
 
-  const handleChange = (event, choreId) => {
-    const eTarget = event.target.value;
-    setParentChores((prev) => {
-      const prevMap = prev.map((elem) => {
-        if (elem.id === choreId) {
-          const newElem = { ...elem, name: eTarget };
-          updateChore(dashboardId, newElem);
-          return newElem;
-        }
-        return elem;
-      });
-      return prevMap;
-    });
-  };
+	useEffect(() => {
+		axios
+			.get(`/dashboards/${dashboardId}/chores/`)
+			.then((res) => {
+				const allChores = res.data;
+				const doneChores = res.data.filter((elem) => elem.done);
+				const progressRatio = (100 * doneChores.length) / allChores.length;
+				setParentChores(allChores);
+				setParentDone(doneChores);
+				setParentProgress({
+					total: allChores.length,
+					completed: doneChores.length,
+					progress: progressRatio,
+				});
+				axios.get(`/dashboards/${dashboardId}/users`).then((res) => {
+					setParentUsers(res.data);
+				});
+			})
+			.catch((err) => console.log('CHORES COMPONENT ERROR', err));
+	}, [broadcast.chores]);
 
-  let trophy = null;
+	const handleToggle = (value) => () => {
+		value.done = !value.done;
+		axios
+			.patch(`/dashboards/${dashboardId}/chores/${value.id}`, value)
+			.then((res) => {
+				setParentChores((prev) => {
+					return prev.map((elem) => {
+						if (elem.id === value.id) return res.data;
+						return elem;
+					});
+				});
+			})
+			.then((res) => {
+				setParentDone(parentChores.filter((elem) => elem.done));
+			})
+			.then(() => {
+				setParentProgress({ ...parentProgress, current: parentDone.length });
+			})
+			.catch((err) => console.log('handleToggle ERROR', err));
+	};
 
-  if (parentDone.length === parentChores.length) {
-    trophy = (
-      <i
-        id="chores-trophy"
-        className="fas fa-trophy fa-3x"
-      ></i>
-    );
-  } else {
-    trophy = null;
-  }
+	const updateChore = (dashboardId, chore) => {
+		axios
+			.patch(`/dashboards/${dashboardId}/chores/${chore.id}`, chore)
+			.catch((err) => console.log('ERROR UPDATING CHORE', err));
+	};
 
-  return (
-    <div className="chore-item">
-      <div style={{ display: 'flex', justifyContent: 'center' }}>{trophy}</div>
-      <List>
-        {parentChores.map((value) => {
-          const labelId = `checkbox-list-label-${value.id}`;
-          // Conditional rendering for ListIem
-          // PrimaryElement : entire line item component
-          // Conditional rendering for the Chip/Completed Checkmark/User Selector
-          // SecondarySwitcher : switches between chip & user selector
-          // SecondaryElement  : toggles completed checkmark
-          let primaryElement = null;
-          let secondarySwitcher = null;
-          let secondaryElement = null;
+	const clearChip = (chip) => {
+		setParentChores((prev) => {
+			return prev.map((elem) => {
+				if (elem.id === chip.id) {
+					return { ...elem, name: 'none' };
+				}
+				return elem;
+			});
+		});
+	};
 
-          if (value.name === 'none') {
-            secondarySwitcher = (
-              <FormControl size="small" className="chore-selector" variant="outlined">
-                <InputLabel id="demo-simple-select-outlined-label">Users</InputLabel>
-                <Select
-                  labelId="demo-simple-select-outlined-label"
-                  id="demo-simple-select-outlined"
-                  defaultValue=""
-                  onChange={(e) => handleChange(e, value.id)}
-                >
-                  {parentUsers.map((user) => {
-                    return (
-                      <MenuItem value={`${user.first_name} ${user.last_name}`}>
-                        {`${user.first_name} ${user.last_name}`}
-                      </MenuItem>
-                    );
-                  })}
-                </Select>
-              </FormControl>
-            );
-          } else {
-            secondarySwitcher = (
-              <Chip
-                name={String(value.name.split(' ').splice(0, 1))}
-                chipValue={value}
-                clearChip={clearChip}
-              />
-            );
-          }
+	const handleChange = (event, choreId) => {
+		const eTarget = event.target.value;
+		setParentChores((prev) => {
+			const prevMap = prev.map((elem) => {
+				if (elem.id === choreId) {
+					const newElem = { ...elem, name: eTarget };
+					updateChore(dashboardId, newElem);
+					return newElem;
+				}
+				return elem;
+			});
+			return prevMap;
+		});
+	};
 
-          if (!value.done) {
-            primaryElement = (
-              <div>
-                <ListItem
-                  role={undefined}
-                  style={{ display: 'flex', justifyContent: 'space-between' }}
-                >
-                  <div style={{ display: 'flex', alignItems: 'center' }}>
-                    <ListItemIcon>
-                        <Checkbox
-                          className="chore-checkbox"
-                          checked={value.done}
-                          inputProps={{ 'aria-labelledby': labelId }}
-                          onClick={handleToggle(value)}
-                        />
-                    </ListItemIcon>
-                    <ListItemText id={labelId} primary={`${value.text}`} />
-                  </div>
-                  <div>
-                    <ListItemSecondaryAction>
-                      <div style={{ display: 'flex', alignItems: 'center' }}>
-                        {secondarySwitcher}
-                        {secondaryElement}
-                      </div>
-                    </ListItemSecondaryAction>
-                  </div>
-                </ListItem>
-              </div>
-            );
-          } else {
-            primaryElement = null;
-          }
+	let trophy = null;
 
-          return <div key={`list-c${value.id}`}>{primaryElement}</div>;
-        })}
-      </List>
-    </div>
-  );
+	if (parentDone.length === parentChores.length) {
+		trophy = <i id="chores-trophy" className="fas fa-trophy fa-3x"></i>;
+	} else {
+		trophy = null;
+	}
+
+	return (
+		<div className="chore-item">
+			<div style={{ display: 'flex', justifyContent: 'center' }}>{trophy}</div>
+			<List>
+				{parentChores.map((value) => {
+					const labelId = `checkbox-list-label-${value.id}`;
+					// Conditional rendering for ListIem
+					// PrimaryElement : entire line item component
+					// Conditional rendering for the Chip/Completed Checkmark/User Selector
+					// SecondarySwitcher : switches between chip & user selector
+					// SecondaryElement  : toggles completed checkmark
+					let primaryElement = null;
+					let secondarySwitcher = null;
+					let secondaryElement = null;
+
+					if (value.name === 'none') {
+						secondarySwitcher = (
+							<FormControl
+								size="small"
+								className="chore-selector"
+								variant="outlined"
+							>
+								<InputLabel id="demo-simple-select-outlined-label">
+									Users
+								</InputLabel>
+								<Select
+									labelId="demo-simple-select-outlined-label"
+									id="demo-simple-select-outlined"
+									defaultValue=""
+									onChange={(e) => handleChange(e, value.id)}
+								>
+									{parentUsers.map((user) => {
+										return (
+											<MenuItem value={`${user.first_name} ${user.last_name}`}>
+												{`${user.first_name} ${user.last_name}`}
+											</MenuItem>
+										);
+									})}
+								</Select>
+							</FormControl>
+						);
+					} else {
+						secondarySwitcher = (
+							<Chip
+								name={String(value.name.split(' ').splice(0, 1))}
+								chipValue={value}
+								clearChip={clearChip}
+							/>
+						);
+					}
+
+					if (!value.done) {
+						primaryElement = (
+							<div>
+								<ListItem
+									role={undefined}
+									style={{ display: 'flex', justifyContent: 'space-between' }}
+								>
+									<div style={{ display: 'flex', alignItems: 'center' }}>
+										<ListItemIcon>
+											<Checkbox
+												className="chore-checkbox"
+												checked={value.done}
+												inputProps={{ 'aria-labelledby': labelId }}
+												onClick={handleToggle(value)}
+											/>
+										</ListItemIcon>
+										<ListItemText id={labelId} primary={`${value.text}`} />
+									</div>
+									<div>
+										<ListItemSecondaryAction>
+											<div style={{ display: 'flex', alignItems: 'center' }}>
+												{secondarySwitcher}
+												{secondaryElement}
+											</div>
+										</ListItemSecondaryAction>
+									</div>
+								</ListItem>
+							</div>
+						);
+					} else {
+						primaryElement = null;
+					}
+
+					return <div key={`list-c${value.id}`}>{primaryElement}</div>;
+				})}
+			</List>
+		</div>
+	);
 }
--- frontend/src/components/widgets/Chores/DoneChores.js
+++ frontend/src/components/widgets/Chores/DoneChores.js
@@ -12,105 +12,104 @@ import ClearIcon from '@material-ui/icons/Clear';
 import DoneIcon from '@material-ui/icons/Done';
 
 export default function DoneChores(props) {
-  const [cookies] = useCookies(['userID']);
-  const dashboardId = cookies.dashboardId;
-  // const parentUsers = [...props.choreState.dashboardUsers];
-  const setParentUsers = props.choreState.setDashboardUsers;
-  // const parentChores = [...props.choreState.choresList];
-  const setParentChores = props.choreState.setChoresList;
-  const parentDone = [...props.choreState.doneList];
-  const setParentDone = props.choreState.setDoneList;
-  const parentProgress = { ...props.choreState.progress };
-  const setParentProgress = props.choreState.setProgress;
+	const [cookies] = useCookies(['userID']);
+	const dashboardId = cookies.dashboardId;
+	// const parentUsers = [...props.choreState.dashboardUsers];
+	const setParentUsers = props.choreState.setDashboardUsers;
+	// const parentChores = [...props.choreState.choresList];
+	const setParentChores = props.choreState.setChoresList;
+	const parentDone = [...props.choreState.doneList];
+	const setParentDone = props.choreState.setDoneList;
+	const parentProgress = { ...props.choreState.progress };
+	const setParentProgress = props.choreState.setProgress;
 
-  useEffect(() => {
-    axios
-      .get(`/dashboards/${dashboardId}/chores/`)
-      .then((res) => {
-        setParentChores(res.data);
-        setParentProgress({ ...parentProgress, total: res.data.length });
-        setParentDone(res.data.filter((elem) => elem.done));
+	useEffect(() => {
+		axios
+			.get(`/dashboards/${dashboardId}/chores/`)
+			.then((res) => {
+				setParentChores(res.data);
+				setParentDone(res.data.filter((elem) => elem.done));
 
-        axios.get(`/dashboards/${dashboardId}/users`).then((res) => {
-          setParentUsers(res.data);
-        });
-      })
-      .catch((err) => console.log('CHORES COMPONENT ERROR', err));
-  }, []);
+				axios.get(`/dashboards/${dashboardId}/users`).then((res) => {
+					setParentUsers(res.data);
+				});
+			})
+			.catch((err) => console.log('CHORES COMPONENT ERROR', err));
+	}, []);
 
-  const handleToggle = (value) => () => {
-    value.done = !value.done;
-    axios
-      .patch(`/dashboards/${dashboardId}/chores/${value.id}`, value)
-      .then((res) => {
-        setParentChores((prev) => {
-          return prev.map((elem) => {
-            if (elem.id === value.id) return res.data;
-            return elem;
-          });
-        });
-      })
-      .then(() => {
-        const newDone = [...parentDone];
-        setParentDone(newDone.filter((elem) => elem.done));
-      })
-      .catch((err) => console.log('handleToggle ERROR', err));
-  };
+	const handleToggle = (value) => () => {
+		value.done = !value.done;
+		axios
+			.patch(`/dashboards/${dashboardId}/chores/${value.id}`, value)
+			.then((res) => {
+				setParentChores((prev) => {
+					return prev.map((elem) => {
+						if (elem.id === value.id) return res.data;
+						return elem;
+					});
+				});
+			})
+			.then(() => {
+				const newDone = [...parentDone];
+				setParentDone(newDone.filter((elem) => elem.done));
+			})
+			.catch((err) => console.log('handleToggle ERROR', err));
+	};
 
-  return (
-    <div className="chore-item chore-completed">
-      <List className={null}>
-        {parentDone.map((value) => {
-          const labelId = `checkbox-list-label-${value.id}`;
-          // Conditional rendering for ListIem
-          // PrimaryElement : entire line item component
-          // Conditional rendering for the Completed Checkmark
-          // SecondaryElement  : toggles completed checkmark
-          let primaryElement = null;
-          let secondaryElement = null;
+	return (
+		<div className="chore-item chore-completed">
+			<List className={null}>
+				{parentDone.map((value) => {
+					const labelId = `checkbox-list-label-${value.id}`;
+					// Conditional rendering for ListIem
+					// PrimaryElement : entire line item component
+					// Conditional rendering for the Completed Checkmark
+					// SecondaryElement  : toggles completed checkmark
+					let primaryElement = null;
+					let secondaryElement = null;
 
-          if (value.done) {
-            secondaryElement = (
-              <IconButton
-                edge="end"
-                aria-label="done"
-                className="chore-check-icon"
-              >
-                <DoneIcon />
-              </IconButton>
-            );
+					if (value.done) {
+						secondaryElement = (
+							<IconButton
+								edge="end"
+								aria-label="done"
+								className="chore-check-icon"
+							>
+								<DoneIcon />
+							</IconButton>
+						);
 
-            primaryElement = (
-              <>
-                <ListItem
-                  role={undefined}
-                  style={{ display: 'flex', justifyContent: 'space-between' }}
-                >
-                  <div style={{ display: 'flex', alignItems: 'center' }}>
-                    <ListItemIcon>
-                      <ClearIcon
-                        checked={value.done}
-                        className="chore-clear-icon"
-                        onClick={handleToggle(value)}
-                      />
-                    </ListItemIcon>
-                    <ListItemText id={labelId} primary={`${value.text}`} />
-                  </div>
-                  <div>
-                    <ListItemSecondaryAction>
-                      <div style={{ display: 'flex', alignItems: 'center' }}>
-                        {secondaryElement}
-                      </div>
-                    </ListItemSecondaryAction>
-                  </div>
-                </ListItem>
-              </>
-            );
-          }
+						primaryElement = (
+							<>
+								<ListItem
+									role={undefined}
+									style={{ display: 'flex', justifyContent: 'space-between' }}
+								>
+									<div style={{ display: 'flex', alignItems: 'center' }}>
+										<ListItemIcon>
+											<ClearIcon
+												checked={value.done}
+												className="chore-clear-icon"
+												onClick={handleToggle(value)}
+											/>
+										</ListItemIcon>
+										<ListItemText id={labelId} primary={`${value.text}`} />
+									</div>
+									<div>
+										<ListItemSecondaryAction>
+											<div style={{ display: 'flex', alignItems: 'center' }}>
+												{secondaryElement}
+											</div>
+										</ListItemSecondaryAction>
+									</div>
+								</ListItem>
+							</>
+						);
+					}
 
-          return <div key={value.id}>{primaryElement}</div>;
-        })}
-      </List>
-    </div>
-  );
+					return <div key={value.id}>{primaryElement}</div>;
+				})}
+			</List>
+		</div>
+	);
 }
